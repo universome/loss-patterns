620d2d579142204dda916407a8465b62af4f95df
Ivan Skorokhodov <iskorokhodov@gmail.com>
2019-06-07 13:33:44 +0300

    feat: better colors for activations entropy

--- src/plotting_utils.py
+++ src/plotting_utils.py
@@ -19,10 +19,11 @@ def generate_linerp_plot(linerp_vals_train, linerp_vals_test, title:str=''):
 def generate_acts_entropy_linerp_plot(linerp_values):
     linerp_values = np.array(linerp_values)
     xs = np.linspace(0, 1, linerp_values.shape[1])
+    colors = plt.cm.jet(np.linspace(0, 1, linerp_values.shape[0]))
 
     fig = plt.figure(figsize=(12, 7))
     for i, layer_entropies in enumerate(linerp_values):
-        plt.plot(xs, layer_entropies, label='Layer #%d' % i)
+        plt.plot(xs, layer_entropies, label='Layer #%d' % i, color=colors[i])
     plt.legend()
     plt.xlabel('alpha')
     plt.grid()
